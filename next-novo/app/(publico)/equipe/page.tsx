76fb1f2d38e48bf395117ad16966cdbe925af9ac
SofiaNogbt <sofianogueirabatista@gmail.com>
2026-06-26 17:03:41 -0300

    [ATT] Dados e fotos

--- next-novo/app/(publico)/equipe/page.tsx
+++ next-novo/app/(publico)/equipe/page.tsx
@@ -6,63 +6,44 @@ export default function Equipe(){
     return(
         <main>
             
-            <div className="Santanas">
+            <div className="Santana">
                 <div className='conteudo'>
-                <div className="icon"><img src="/01.jpg" alt="Personagem" /></div>
+                <div className="icon"><img src="/02.png" alt="Personagem" /></div>
                 <div className="info">
                 <p className='titulo-personagem'>Maria Eduarda de Jesus Santana</p>
-                <p className= 'texto-info'>Que vai me ver em todo lugar.
-                    Vai desenhar meu olhar no céu
-                    Que meu amor tem gosto de mel
-                    Parei de brincar, só vou se você
-                    Meu patuá, meu azul-pincel
-                    Pra me pintar de colorido
-                    Mas é só me soltar, que eu volto a ser
-                    Serpente no mar profundo
-                    É que eu 'to com medo
-                    É que eu não sou pura
-                    Você vai se entregar, e eu vou te amar
-                    Mas eu não quero ser sua</p>
+                <p className= 'texto-info'> Graduanda em Sistemas de Informação Bacharelado pela Universidade Federal de Itajubá - UNIFEI. <br></br>
+                <br></br>
+                💻 - <Link className= 'link01' href='https://github.com/MSantanas' target="_blank">GitHub</Link>
+                </p>
                 </div>
                 </div>
             </div>
 
-            <div className="Mathos">
+            <div className="Matheus">
                 <div className='conteudo'>
                 <div className="icon"><img src="/02.jpg" alt="Personagem" /></div>
                 <div className="info">
                 <p className="titulo-personagem">Matheus Motta Soriano</p>
-                <p className="texto-info">Se droga fosse álcool eu morria de cirrose
-                Se álcool fosse droga eu morria de overdose
-
-                Sou vampiro doidão, sou o vampiro doidão
-                Eu to muito louco, etá baseado do bom!
-
-                Estava no escurinho, comendo a empregada
-                alguém abriu a porta e eu comi a bunda errada
-
-                Eu sou o vampiro doidão, eu sou o vampiro doidão
-                Só faço sexo dentro do caixão</p>
+                <p className="texto-info">Graduando em Sistemas de Informação Bacharelado pela Universidade Federal de Itajubá - UNIFEI. Técnico em TI pelo IFSuldeMinas <br></br>
+                <br></br>
+                💻 - <Link className= 'link02' href='https://github.com/SorianoMM256' target="_blank">GitHub</Link> <br></br>
+                💡 - <Link className= 'link03' href='https://www.linkedin.com/in/matheus-motta-soriano-2a179b312/' target="_blank">LinkedIn</Link></p>
                 </div>
                 </div>
             </div>
 
             
 
-            <div className="OHomiDasCarcaErguida">
+            <div className="Sofia">
                 <div className='conteudo'>
-                <div className="icon"><img src="/03.jpg" alt="Personagem" /></div>
+                <div className="icon"><img src="/03.png" alt="Personagem" /></div>
                 <div className="info">
                 <p className="titulo-personagem">Sofia Nogueira Batista</p>
-                <p className="texto-info">Tu me traíste com o preto do cigano
-                Agora vou te arrebentar o cu com o cano
-                Vou te partir as teclas desse piano
-                E a tua cona cheira a frango
-
-                És uma putinha da vida
-                Que só quer é putaria
-                Mas agora apanhaste SIDA
-                E quem fuder contigo é suicida</p>
+                <p className="texto-info">Graduanda em Sistemas de Informação Bacharelado pela Universidade Federal de Itajubá - UNIFEI.<br></br>
+                <br></br>
+                💻 - <Link className= 'link04' href='https://github.com/SofiaNogbt' target="_blank" >GitHub</Link> <br></br>
+                💡 - <Link className= 'link05' href='https://www.linkedin.com/in/sofia-nogueira-batista-740870381/' target="_blank">LinkedIn</Link> <br></br>
+                🎮 - <Link className= 'link06' href='https://sunset-sky.itch.io' target="_blank">itch.io</Link></p>
                 </div>
                 </div>
             </div>
