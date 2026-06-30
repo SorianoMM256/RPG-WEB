78ab321ee3da51cf49879277d98b0d1fb37a67a8
MSantanas <mariaesantana2006@gmail.com>
2026-06-30 16:33:51 -0300

    Fix -> alterando o tipo da senha de text para password

--- next-novo/app/(publico)/criarconta/page.tsx
+++ next-novo/app/(publico)/criarconta/page.tsx
@@ -1,24 +1,23 @@
 import "../globals.css";
-import "./criar.css"
+import "./criar.css";
 
-export default function Login(){
-    return(
-        <main>
-            
-            <div className="CriarBox">
-                <img src= "/teste.png" id="logo"></img>
-                <label htmlFor="nome">Nome:</label>
-                <input type='text' id="nome" name = 'nome'></input>
-                <label htmlFor="email">Email:</label>
-                <input type='text' id="email" name = 'email'></input>
-                <label htmlFor="senha">Senha:</label>
-                <input type='text' id="senha" name = 'senha'></input>
-                <label htmlFor="confirmacao">Confirme a senha:</label>
-                <input type='text' id="confirmacao" name = 'confirmacao'></input>
+export default function Login() {
+  return (
+    <main>
+      <div className="CriarBox">
+        <img src="/teste.png" id="logo"></img>
+        <label htmlFor="nome">Nome:</label>
+        <input type="text" id="nome" name="nome"></input>
+        <label htmlFor="email">Email:</label>
+        <input type="text" id="email" name="email"></input>
+        <label htmlFor="senha">Senha:</label>
+        <input type="password" id="senha" name="senha"></input>
+        <label htmlFor="confirmacao">Confirme a senha:</label>
+        <input type="password" id="confirmacao" name="confirmacao"></input>
 
-                <button className="btncriar">Criar</button>
-                <button className="btncancelar">Cancelar</button>
-            </div>
-        </main>
-    );
+        <button className="btncriar">Criar</button>
+        <button className="btncancelar">Cancelar</button>
+      </div>
+    </main>
+  );
 }
--- next-novo/app/(publico)/login/page.tsx
+++ next-novo/app/(publico)/login/page.tsx
@@ -1,20 +1,19 @@
 import "../globals.css";
-import "./login.css"
+import "./login.css";
 
-export default function Login(){
-    return(
-        <main>
+export default function Login() {
+  return (
+    <main>
+      <div className="LoginBox">
+        <img src="/teste.png" id="logo"></img>
+        <label htmlFor="loginemail">Email:</label>
+        <input type="text" id="loginemail" name="email"></input>
+        <label htmlFor="loginsenha">Senha:</label>
+        <input type="password" id="loginsenha" name="senha"></input>
 
-            <div className="LoginBox">
-                <img src= "/teste.png" id="logo"></img>
-                <label htmlFor="loginemail">Email:</label>
-                <input type='text' id="loginemail" name = 'email'></input>
-                <label htmlFor="loginsenha">Senha:</label>
-                <input type='text' id="loginsenha" name = 'senha'></input>
-
-                <button className="btnlogar">Entrar</button>
-                <button className="btncancelarlogin">Cancelar</button>
-            </div>
-        </main>
-    );
+        <button className="btnlogar">Entrar</button>
+        <button className="btncancelarlogin">Cancelar</button>
+      </div>
+    </main>
+  );
 }
