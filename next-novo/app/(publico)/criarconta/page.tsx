d8ae8a66be417c27f4f035fe968aaedf1dcfe83e
MSantanas <mariaesantana2006@gmail.com>
2026-07-04 15:19:44 -0300

    [FIX] Mudando os inputs para dentro de "forms" em criar conta

--- next-novo/app/(publico)/criarconta/page.tsx
+++ next-novo/app/(publico)/criarconta/page.tsx
@@ -6,17 +6,18 @@ export default function Login() {
     <main>
       <div className="CriarBox">
         <img src="/teste.png" id="logo"></img>
-        <label htmlFor="nome">Nome:</label>
-        <input type="text" id="nome" name="nome"></input>
-        <label htmlFor="email">Email:</label>
-        <input type="text" id="email" name="email"></input>
-        <label htmlFor="senha">Senha:</label>
-        <input type="password" id="senha" name="senha"></input>
-        <label htmlFor="confirmacao">Confirme a senha:</label>
-        <input type="password" id="confirmacao" name="confirmacao"></input>
-
-        <button className="btncriar">Criar</button>
-        <button className="btncancelar">Cancelar</button>
+        <form>
+          <label htmlFor="nome">Nome:</label>
+          <input type="text" id="nome" name="nome"></input>
+          <label htmlFor="email">Email:</label>
+          <input type="text" id="email" name="email"></input>
+          <label htmlFor="senha">Senha:</label>
+          <input type="password" id="senha" name="senha"></input>
+          <label htmlFor="confirmacao">Confirme a senha:</label>
+          <input type="password" id="confirmacao" name="confirmacao"></input>
+          <button className="btncriar">Criar</button>
+          <button className="btncancelar">Cancelar</button>
+        </form>
       </div>
     </main>
   );
