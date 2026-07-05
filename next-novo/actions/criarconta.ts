7b3b71836e4ac10e622ab8d1d9814aa123e0aee1
MSantanas <mariaesantana2006@gmail.com>
2026-07-05 12:49:17 -0300

    [FIX] consertando tipo do retorno (FormState) e redundancias

--- next-novo/actions/criarconta.ts
+++ next-novo/actions/criarconta.ts
@@ -3,17 +3,31 @@
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcrypt";
 
-export async function criarConta(formData: FormData) {
-  "use server";
+export type FormState = {
+  msg: string;
+  fields: {
+    nome: string;
+    email: string;
+  };
+};
 
+//precisa ser | undefined pra qnd a pagina iniciar!
+export async function criarConta(
+  state: FormState | undefined,
+  formData: FormData,
+): Promise<FormState> {
   //pegando informacoes!
-  const nome = formData.get("nome")?.toString();
-  const email = formData.get("email")?.toString();
-  const senha = formData.get("senha")?.toString();
-  const confirmacao = formData.get("confirmacao")?.toString();
+  const nome = formData.get("nome")?.toString() || "";
+  const email = formData.get("email")?.toString() || "";
+  const senha = formData.get("senha")?.toString() || "";
+  const confirmacao = formData.get("confirmacao")?.toString() || "";
+
+  //cria para quando retornar (caso dê erro) as infos preenchidas
+  const fields = { nome, email };
 
   if (!email || !nome || !senha || !confirmacao) {
-    return { msg: "Preencha todas as informações!" };
+    //retorna fields e msg pro frontend
+    return { msg: "Preencha todas as informações!", fields };
   }
 
   // 1. verificar se o email ja ta cadastrado
@@ -24,28 +38,30 @@ export async function criarConta(formData: FormData) {
   if (usuarioExistente) {
     return {
       msg: "Email já está em uso! Adicione outro.",
+      fields, //sempre retorna o fields!
     };
   }
   //verificar se ta tudo preenchido
-  if (senha == confirmacao) {
-    if (nome) {
-      const senhaCriptograda = await bcrypt.hash(senha!, 10);
-
-      await prisma.user.create({
-        data: {
-          email: email,
-          name: nome,
-          password: senhaCriptograda,
-        },
-      });
-
-      return {
-        msg: `Bem-vindo ${nome}, conta criada!`,
-      };
-    }
-  } else {
+  if (senha != confirmacao) {
     return {
       msg: "As senhas estão incompatíveis.",
+      fields,
     };
   }
+
+  //passou por todas as verificacoes chega aqui:
+  const senhaCriptograda = await bcrypt.hash(senha, 10);
+
+  await prisma.user.create({
+    data: {
+      email: email,
+      name: nome,
+      password: senhaCriptograda,
+    },
+  });
+
+  return {
+    msg: `Bem-vindo ${nome}, conta criada!`,
+    fields: { nome: "", email: "" }, //zerando o campo!
+  };
 }
