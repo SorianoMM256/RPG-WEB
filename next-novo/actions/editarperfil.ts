2828d3bcf9014d0f6ba017c4ac67d9822b5f6562
MSantanas <mariaesantana2006@gmail.com>
2026-07-09 16:39:51 -0300

    [ADD] A funcao agora pegam as fotos

--- next-novo/actions/editarperfil.ts
+++ next-novo/actions/editarperfil.ts
@@ -1,7 +1,13 @@
 "use server";
 
 import { prisma } from "@/lib/prisma";
+
+/*A versão padrão do fs não aceita await (Promises); 
+ela espera que você passe uma função de resposta (callback) como 3º ou 4º argumento. 
+p usar com await, precisamos importar a versão moderna baseada em Promises.*/
+import { writeFile } from "fs/promises";
 import { cookies } from "next/headers";
+import path from "path";
 
 //como vamos editar o perfil, a msg do status da operacao
 export type FormState = {
@@ -18,6 +24,8 @@ export async function editarPerfil(
   //as string significa: eu juro que o que está vindo desse input é um texto, pode confiar e tirar esse aviso de erro
   const nome = formData.get("nome") as string | null;
   const email = formData.get("email") as string | null;
+
+  const arqFoto = formData.get("foto") as File | null;
 
   // 1. pega a informacao do usuario logado
   const cookie = await cookies();
@@ -36,11 +44,38 @@ export async function editarPerfil(
     };
   }
 
+  let urlFoto: string | undefined = undefined;
+
+  //o >0 significa que: qnd nada eh selecionado, um file vazio vem como obj, e o tamanho em bytes eh 0
+  if (arqFoto && arqFoto.size > 0) {
+    try {
+      //recebe o arquivo como file e transforma pra buffer: dados binarios
+      const bytes = await arqFoto.arrayBuffer();
+      const buffer = Buffer.from(bytes);
+
+      //toda essa coisa feia existe por 1 motivo: NAO REPETIR o nome do arquivo e puxar errado!
+      // nome do usuario + o horario de criacao + o tipo da imagme (jpg, png..)
+      const nomeArq = `${nomeUs}-${Date.now()}${path.extname(arqFoto.name)}`;
+
+      //aqui mostra onde salvar!
+      //o cwd significa current working directory, descobrindo onde o projeto está
+      //path.join junta os pedacos, formatando com barras!
+      const caminho = path.join(process.cwd(), "public", "uploads", nomeArq);
+
+      await writeFile(caminho, buffer);
+      urlFoto = `/uploads/${nomeArq}`;
+    } catch (error) {
+      console.error(error);
+      return { msg: "Erro ao salvar foto de pefil!", sucesso: false };
+    }
+  }
+
   // 3.5 Cria um obj de dados dinamico
-  const dadosUsuario: { nome?: string; email?: string } = {};
+  const dadosUsuario: { nome?: string; email?: string; foto?: string } = {};
   //se algum deles foi fornecido no formulario...
   if (nome) dadosUsuario.nome = nome;
   if (email) dadosUsuario.email = email;
+  if (urlFoto) dadosUsuario.foto = urlFoto;
 
   try {
     await prisma.user.update({
